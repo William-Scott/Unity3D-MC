f8e841761f26768deac54f5e294058c476fe979f
William-Scott <wzq499538062@gmail.com>
2017-11-19 10:28:04 +0800

    Add files via upload

---
+++ PlayerSetup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerSetup : NetworkBehaviour {
+
+    [SerializeField] Behaviour[] NeedToBeDisable;
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer) {
+            for (int i = 0; i < NeedToBeDisable.Length; i++)
+            {
+                NeedToBeDisable[i].enabled = false;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
